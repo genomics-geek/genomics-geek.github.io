72da1764e2fe718b7cbe4340d81d83ac518646fa
Michael A. Gonzalez <genomics.geek.04.22@gmail.com>
2018-12-12 09:49:08 -0500

    Added initial layout for github pages

---
+++ src/pages/about/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Layout from '../../components/layout'
+
+const About = () => (
+  <Layout title="About">
+    <p>About coming soon!</p>
+  </Layout>
+)
+
+export default About
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,18 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Container, Header, Segment } from 'semantic-ui-react'
 
 import 'semantic-ui-css/semantic.min.css'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
-import SEO from '../components/seo'
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+  <Layout title="Home">
+    <Segment vertical inverted textAlign="center" className="masthead">
+      <Container text>
+        <Header inverted as="h1">Genomics Geek Website</Header>
+        <Header inverted as="h2">Genetics. Software. Bioinformatics.</Header>
+      </Container>
+    </Segment>
   </Layout>
 )
 
